9de09f1ff04a3041e95b664108beaf6b77fa1598
Nick Henderson <nch8501@rit.edu>
2018-10-26 12:56:00 -0400

    Finished part C

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -3,13 +3,13 @@ const models = require('../models');
 const Domo = models.Domo;
 
 const makerPage = (req, res) => {
-  Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) =>{
-    if(err){
+  Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
+    if (err) {
       console.log(err);
-      return res.status(400).json({error: 'An error occurred'});
+      return res.status(400).json({ error: 'An error occurred' });
     }
-    
-    return res.render('app', {domos: docs});
+
+    return res.render('app', { csrfToken: req.csrfToken(), domos: docs });
   });
 };
 
