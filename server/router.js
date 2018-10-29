58d063fed5d1e26bdac3cd5705e57e160c5f722a
Nick Henderson <nch8501@rit.edu>
2018-10-29 18:42:07 -0400

    On page 10 of Part D

--- server/router.js
+++ server/router.js
@@ -3,9 +3,9 @@ const mid = require('./middleware');
 
 // routes to go to
 const router = (app) => {
+  app.get('/getToken', mid.requiresSecure, controllers.Account.getToken);
   app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
   app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
-  app.get('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signupPage);
   app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
   app.get('/logout', mid.requiresLogin, controllers.Account.logout);
   app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
